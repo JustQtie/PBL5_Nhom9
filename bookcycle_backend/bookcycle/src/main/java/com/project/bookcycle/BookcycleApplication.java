40a1dac31e501eeabcabd70ff9e3f9476d20feab
MinhIt2k3 <nhatminhhoangeng22112k3@gmail.com>
2024-03-29 15:57:58 +0700

    init project's backend

---
+++ bookcycle_backend/bookcycle/src/main/java/com/project/bookcycle/BookcycleApplication.java
@@ -0,0 +1,13 @@
+package com.project.bookcycle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookcycleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookcycleApplication.class, args);
+	}
+
+}
