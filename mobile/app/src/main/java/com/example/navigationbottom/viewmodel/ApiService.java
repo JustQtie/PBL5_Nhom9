cabecbacbb369c3d0ea1868981d4c2d59618a415
HoVanThao <hovanthao0611cs@gmail.com>
2024-06-03 09:24:41 +0700

    update 3/6

--- mobile/app/src/main/java/com/example/navigationbottom/viewmodel/ApiService.java
+++ mobile/app/src/main/java/com/example/navigationbottom/viewmodel/ApiService.java
@@ -2,5 +2,5 @@ package com.example.navigationbottom.viewmodel;
 
 public class ApiService {
 
-    public static final String BASE_URL = "https://9236-171-225-184-143.ngrok-free.app/";
+    public static final String BASE_URL = "https://43d0-171-225-184-143.ngrok-free.app/";
 }
