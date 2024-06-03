00e15349b4ef30fc1800e8c8694eab69b34ebe3e
HoVanThao <hovanthao0611cs@gmail.com>
2024-06-03 11:56:01 +0700

    update chat mobile

--- frontend-admin-reactjs/src/components/featured/Featured.jsx
+++ frontend-admin-reactjs/src/components/featured/Featured.jsx
@@ -1,29 +1,55 @@
-import "./featured.scss"
+import React, { useEffect, useState } from "react";
+import "./featured.scss";
 import MoreVertIcon from '@mui/icons-material/MoreVert';
 import { CircularProgressbar, buildStyles } from "react-circular-progressbar";
 import "react-circular-progressbar/dist/styles.css";
 import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
 import KeyboardArrowUpOutlinedIcon from '@mui/icons-material/KeyboardArrowUpOutlined';
 import ChangingProgressProvider from "./ChangingProgressProvider";
+import { getAllUsers } from "../../services/apiServices";
+import { toast } from "react-toastify";
 
 const Featured = () => {
+    const [activeUserPercentage, setActiveUserPercentage] = useState(0);
+
+    const fetchListUsers = async () => {
+        try {
+            const token = localStorage.getItem("token");
+            if (!token) {
+                toast.error("Token không được tìm thấy");
+                return;
+            }
+
+            const res = await getAllUsers(token);
+
+            if (res && res.EC === 0) {
+                const users = res.userResponseList;
+                const totalUsers = users.length;
+                const activeUsers = users.filter(user => user.active).length;
+                const percentage = Math.ceil((activeUsers / totalUsers) * 100);
+
+                setActiveUserPercentage(percentage);
+            }
+        } catch (error) {
+            toast.error("Lỗi khi lấy danh sách người dùng", error);
+        }
+    };
+
+    fetchListUsers();
+
     return (
         <div className="featured">
-            {/* <div className="top">
-                <h1 className="title">Số lượng người dùng truy cập hiện tại</h1>
-                <MoreVertIcon fontSize="small" />
-            </div> */}
             <div className="bottom">
                 <div className="featuredChart">
                     <ChangingProgressProvider
-                        values={[0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100]}
+                        values={[activeUserPercentage]}
                     >
                         {(percentage) => (
                             <CircularProgressbar
                                 value={percentage}
                                 text={`${percentage}%`}
                                 styles={buildStyles({
-                                    pathTransitionDuration: 0.95,
+                                    pathTransitionDuration: 0.85,
                                     trailColor: "#82ca9d",
                                     pathColor: "#210876",
                                     textColor: "#737173",
@@ -33,21 +59,20 @@ const Featured = () => {
                     </ChangingProgressProvider>
                 </div>
                 <p className="title">Số lượng người dùng đang hoạt động</p>
-                {/* <p className="amount">500$</p>
-                <p className="desc">Xử lý giao dịch trước đó. Các khoản thanh toán cuối cùng có thể không được đưa vào.</p> */}
+
                 <div className="summary">
                     <div className="item">
-                        <div className="itemTitle">hôm nay</div>
-                        <div className="itemResult negative">
-                            <KeyboardArrowDownIcon fontSize="small" />
-                            <div className="resultAmount">70%</div>
+                        <div className="itemTitle">Hôm nay</div>
+                        <div className="itemResult positive">
+                            <KeyboardArrowUpOutlinedIcon fontSize="small" />
+                            <div className="resultAmount">{activeUserPercentage}%</div>
                         </div>
                     </div>
                     <div className="item">
                         <div className="itemTitle">Hôm qua</div>
                         <div className="itemResult positive">
                             <KeyboardArrowUpOutlinedIcon fontSize="small" />
-                            <div className="resultAmount">60%</div>
+                            <div className="resultAmount">90%</div>
                         </div>
                     </div>
                     <div className="item">
@@ -60,7 +85,7 @@ const Featured = () => {
                 </div>
             </div>
         </div>
-    )
+    );
 }
 
 export default Featured;
